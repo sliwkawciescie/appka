0d45612789991a508103ef8b026bbf9f6239e604
sliwkawciescie <60521793+sliwkawciescie@users.noreply.github.com>
2020-05-27 19:10:36 +0200

    Update PandaApplicationSeleniumTest.java

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -27,10 +27,10 @@ public class PandaApplicationSeleniumTest {
 
         // Tworzymy nową instancję Firefoxa
        // driver = new FirefoxDriver(capabilities);
-	driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), capabilities);
+	driver = new RemoteWebDriver(new URL("http://selenium-hubcompose:4444/wd/hub"), capabilities);
         // Otwieramy stronę
         // Pamiętaj, że aplikacja Spring musi działać! To znaczy też musi być włączona.
-        driver.get("http://172.19.0.3:8080/");
+        driver.get("http://10.0.2.15:8080/");
         // Wyświetlamy informacje, że udało się otwozyć stronę
         System.out.println("Successfully opened the website");
         //Czekamy 2 sekundy
